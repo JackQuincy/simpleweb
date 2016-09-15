68a8ab93df3799e76e61dc08f422abd9ec1f573f
REDMOND\jaquincy <jaquincy@microsoft.com>
2016-09-15 10:33:53 -0700

    basic server listening on port 80

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+	var count int
+	count = 1
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) { 
+		w.Header().Set("Content-Type", "text/html; charset=utf-8") 
+		fmt.Fprintln(w, "<h1>Hello</h1>") 
+		fmt.Fprintln(w, "<br>This server has served", count, " requests")
+		count++ 
+	}) 
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
