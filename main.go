d5497e0a5dc5857bfdd2b50206d7a91c3f33222d
REDMOND\jaquincy <jaquincy@microsoft.com>
2016-09-19 15:12:57 -0700

    ran gofmt
    formatting changes

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	var count int
 	count = 1
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) { 
-		w.Header().Set("Content-Type", "text/html; charset=utf-8") 
-		fmt.Fprintln(w, "<h1>Hello</h1>") 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprintln(w, "<h1>Hello</h1>")
 		fmt.Fprintln(w, "<br>This server has served", count, " requests")
-		count++ 
-	}) 
+		count++
+	})
 	log.Fatal(http.ListenAndServe(":80", nil))
 
 }
-
